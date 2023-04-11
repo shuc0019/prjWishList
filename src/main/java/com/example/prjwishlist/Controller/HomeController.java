dd7332bc5045d25ba837dc723a5cae9885ed0fce
shuc0019 <shuc0019@stud.kea.dk>
2023-04-11 11:58:04 +0200

    lololo

--- src/main/java/com/example/prjwishlist/Controller/HomeController.java
+++ src/main/java/com/example/prjwishlist/Controller/HomeController.java
@@ -3,6 +3,7 @@ package com.example.prjwishlist.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,14 +15,14 @@ public class HomeController {
         return "index";
     }
 
-    @GetMapping("wishlit")
+    @PostMapping("/")
     public String login(String username, String password, Model model) {
         if (username.equals("hej") && password.equals("hej")){
             return "wishlit";
         } else {
-
+            model.addAttribute("lol", "Din login er forkert");
         }
-
+        return "index";
     }
 
 
