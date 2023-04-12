e9dece86589c5c5976fa9819842c4fe29a9b0c32
suusmita <susmidey2001@gmail.com>
2023-04-12 10:56:05 +0200

    added css html

--- src/main/java/com/example/prjwishlist/Controller/HomeController.java
+++ src/main/java/com/example/prjwishlist/Controller/HomeController.java
@@ -4,24 +4,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 @Controller
 public class HomeController {
 
     @GetMapping("/")
     public String index(){
-        return "index";
+        return "login";
     }
 
     @PostMapping("/")
     public String login(String username, String password, Model model) {
         if (username.equals("hej") && password.equals("hej")){
-            return "wishlit";
+            return "loginconfirm";
         } else {
-            model.addAttribute("lol", "Din login er forkert");
+            model.addAttribute("lol", "Din login er forkert ");
         }
+        return "login";
+    }
+
+    // redirect to main page
+    @GetMapping("/index")
+    public String login() {
         return "index";
     }
 
