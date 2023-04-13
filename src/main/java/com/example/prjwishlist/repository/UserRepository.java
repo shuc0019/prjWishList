c8a866065529a08b1873a76a61e7f785e4654cdb
suusmita <susmidey2001@gmail.com>
2023-04-13 10:53:53 +0200

    user repository

--- src/main/java/com/example/prjwishlist/repository/UserRepository.java
+++ src/main/java/com/example/prjwishlist/repository/UserRepository.java
@@ -3,21 +3,41 @@ package com.example.prjwishlist.repository;
 
 import com.example.prjwishlist.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 
 @Repository
 public class UserRepository {
 
     @Autowired
-    JdbcTemplate jdbcTemplate;
+    JdbcTemplate template;
 
     public void addUser(User user) {
         String sql = "INSERT INTO users (username, userpassword) VALUES (?, ?)";
-        jdbcTemplate.update(sql, user.getUsername(), user.getUserpassword());
+        template.update(sql, user.getUsername(), user.getUserpassword());
     }
 
+    public User findUserByUsernameAndPassword(String username, String password) {
+        String sql = "SELECT * FROM users WHERE username = ? AND userpassword = ?";
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
+        List<User> users = template.query(sql, rowMapper, username, password);
+        if (users.size() == 1) {
+            return users.get(0);
+        } else {
+            return null;
+        }
+    }
+
+    public boolean doesTheUserExist(String username) {
+        String sql = "SELECT * FROM users WHERE username = ?";
+        RowMapper<User> rowMapper = new BeanPropertyRowMapper<>(User.class);
+        List<User> users = template.query(sql, rowMapper, username);
+        return !users.isEmpty();
+    }
 
 }
